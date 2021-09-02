0a6921591ada71313a3b86eaf15761c5a75b3931
Jeremy Antoine <jeremincanada@gmail.com>
2021-09-02 15:41:33 -0400

    challenge complete but not good looking front end needed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: false
+
 class Restaurant < ApplicationRecord
   # A restaurant must have a name, an address and a category.
-validates :name, :address, :category, presence: true
+  validates :name, :address, :category, presence: true
 
-# When a restaurant is destroyed, all of its reviews must be destroyed as well
-has_many :reviews, dependent: :destroy
+  # When a restaurant is destroyed, all of its reviews must be destroyed as well
+  has_many :reviews, dependent: :destroy
 
-# A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
-validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"],
-    message: "%{value} is not a valid category" }
+  # A restaurant category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian],
+    message: '%<value>s is not a valid category' }
 end
